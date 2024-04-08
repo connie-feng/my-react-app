72af66b30761e73c99004e69c8bfcd3593b8e37d
Connie Feng <conniefeng28@gmail.com>
2024-04-08 16:21:37 -0500

    Add practice for updating arrays in state with FoodList

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import ColorPicker from './ColorPicker.jsx'
 import Counter from './Counter.jsx'
 import Car from './Car.jsx'
+import FoodList from './FoodList.jsx'
 
 function App() {
 
 
   return (
     <>
+      <FoodList />
       <Car />
       <Counter />
       <ColorPicker />
