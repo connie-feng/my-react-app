bd6885f7447dc1aeaa61d6dd7c75ac789aa9d55a
Connie Feng <conniefeng28@gmail.com>
2024-04-08 16:04:48 -0500

    Add updating objects with useState using a Car

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ColorPicker from './ColorPicker.jsx'
 import Counter from './Counter.jsx'
+import Car from './Car.jsx'
 
 function App() {
 
 
   return (
     <>
+      <Car />
       <Counter />
       <ColorPicker />
     </>
