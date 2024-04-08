0b27631905211b978f95c9cb0c864534605267ad
Connie Feng <conniefeng28@gmail.com>
2024-04-08 15:53:44 -0500

    Add updater function practice with Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import ColorPicker from './ColorPicker.jsx'
+import Counter from './Counter.jsx'
 
 function App() {
 
 
   return (
-    <ColorPicker />
+    <>
+      <Counter />
+      <ColorPicker />
+    </>
   )
 }
 
