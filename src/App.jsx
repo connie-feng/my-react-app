0ace4b7ed301b2202a8e2caa607b0c57803c8abc
Connie Feng <conniefeng28@gmail.com>
2024-04-10 13:50:30 -0500

    Add practice for useContext() hook to avoid prop drilling

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import FoodList from './FoodList.jsx'
 import ToDoList from './ToDoList.jsx'
 import Window from './Window.jsx'
 import DigitalClock from './DigitalClock.jsx'
+import ComponentA from './ComponentA.jsx'
 
 function App() {
 
   return (
     <>
+      <ComponentA />
+
       <DigitalClock />
       <Window />
       <ToDoList />
